b1a078f566d9c465b76d33656a808e7c74e05ec0
mfrugoni <miguelmfrugoni@gmail.com>
2023-06-19 21:26:41 -0300

    V11: Replace bootstrap icons for pngs
    create js files
    
    load cards dinamically with js

---
+++ scripts/data.js
@@ -0,0 +1,61 @@
+console.log('data.js');
+
+const productsData = [
+    {
+        id: 1,
+        title: 'HOGAR',
+        img: './assets/img/hogar.png',
+        alt: 'icono hogar'
+    },
+
+    {
+        id: 2,
+        title: 'AUTOMOTOR',
+        img: './assets/img/auto.png',
+        alt: 'icono auto'
+    },
+
+    {
+        id: 3,
+        title: 'VIDA',
+        img: './assets/img/vida.png',
+        alt: 'icono vida'
+    },
+
+    {
+        id: 4,
+        title: 'COMBINADO FAMILIAR',
+        img: './assets/img/family.png',
+        alt: 'icono familia'
+    },
+
+    {
+        id: 5,
+        title: 'INCENDIO',
+        img: './assets/img/incendio.png',
+        alt: 'icono incendio'
+    },
+
+    {
+        id: 6,
+        title: 'INTEGRAL DE COMERCIO',
+        img: './assets/img/comercio.png',
+        alt: 'icono comercio'
+    },
+
+    {
+        id: 7,
+        title: 'A.R.T.',
+        img: './assets/img/accidente.png',
+        alt: 'icono accidente'
+    },
+
+    {
+        id: 8,
+        title: 'TURISMO AVENTURA',
+        img: './assets/img/caminante.png',
+        alt: 'icono aventura'
+    }
+];
+
+console.log(productsData);
